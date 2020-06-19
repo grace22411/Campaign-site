3fc277ac08ec00df519845ce27896594d906b844
grace22411 <olayinkagrace112.ob@gmail.com>
2020-06-19 10:41:28 -0700

    mobile screens done

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+$(document).ready(function () {
+  
+  $(".fa-times").click(function () {
+    $(".modal-content").fadeOut();
+  });
+
+  $(".fa-bars").click(function () {
+    $(".modal-content").show();
+  })
+
+  $(".fa-times").click(function () {
+    $(".modal-content").fadeOut();
+    $(".success").fadeOut();
+  });
+
+  $(".whyvote").click(function(){
+		var $element = $(".mission").offset().top;
+	
+		$("html,body").animate({
+			scrollTop : $element
+		}, 900);
+	});
+ 
+
+});
+
+
